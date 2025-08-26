e6463bb801f6e34d61cd9ba580a16725e400f620
Rafig Huseynzade <Rafig12332@gmail.com>
2025-08-26 20:39:39 +0400

    chore(lake): place modules under lean/PsiTM and set srcDir; fix roots

--- lakefile.lean
+++ lakefile.lean
@@ -5,9 +5,13 @@ package psi_tm where
 
 @[default_target]
 lean_lib PsiTM where
-  roots := #[`PsiTM.BudgetLemma, `PsiTM.PsiFooling, `PsiTM.LkLowerBound, `PsiTM.AntiSimulationHook]
-  moreLeanArgs := #["-R","lean","PsiTM"]
+  srcDir := "lean"
+  roots  := #[
+    `PsiTM.BudgetLemma,
+    `PsiTM.PsiFooling,
+    `PsiTM.LkLowerBound,
+    `PsiTM.AntiSimulationHook
+  ]
 
 lean_exe psitm where
   root := `Main
-  moreLeanArgs := #["-R","lean","PsiTM"]
